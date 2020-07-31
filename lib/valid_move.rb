77dfe4bcad690be7dde90d3f3d35c42a306f0545
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-07-31 02:07:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,7 +8,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(1,9) && !position_taken?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
      true
     elsif
     false
