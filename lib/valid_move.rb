64d5728b6ea35302fa085ec1a0da53d40f2c7742
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-07-31 01:53:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,15 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken? (board, index)
+  !(board[index].nil? || board[index] == " " || board[index] == "")
+end
+
+def valid_move? (board, index)
+  if index.between(0, 8) && !position_taken?
+    return true
+    elsif
+    return false
+  end
+end
